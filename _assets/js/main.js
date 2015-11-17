9b2f5166c7eb92bf19125b757e12d90cf9af48b3
Vítor Teixeira <vsdteixeira@gmail.com>
2015-11-17 19:18:18 +0000

    Build site at 2015-11-17 19:18:18 UTC

--- _assets/js/main.js
+++ _assets/js/main.js
@@ -3,8 +3,10 @@
 //= require vendor/ink/ink.spy.js
 //= require vendor/ink/ink.sticky.js
 //= require vendor/ink/ink.smoothscroller.js
+//= require vendor/ink/ink.drawer.js
 
-Ink.requireModules( ['Ink.Dom.Selector_1','Ink.UI.Spy_1','Ink.UI.Sticky_1','Ink.UI.SmoothScroller_1'], function( Selector, Spy, Sticky, SmoothScroller ){
+
+Ink.requireModules( ['Ink.Dom.Selector_1','Ink.UI.Spy_1','Ink.UI.Sticky_1','Ink.UI.SmoothScroller_1', 'Ink.UI.Drawer_1'], function( Selector, Spy, Sticky, SmoothScroller, Drawer ){
 
     // Get current section and highlight it in the navbar
     var Menu = Ink.i('top-menu');
@@ -15,12 +17,14 @@ Ink.requireModules( ['Ink.Dom.Selector_1','Ink.UI.Spy_1','Ink.UI.Sticky_1','Ink.
     }
 
     // Make the navbar stick to the top
-    new Sticky('#top-menu', { activateInLayouts: ['tiny', 'small', 'medium', 'large', 'xlarge'] });
+    new Sticky('#top-menu', { activateInLayouts: ['medium', 'large', 'xlarge'] });
 
-    
-    
+
+
     // Smooth Scroll for homepage sections
     SmoothScroller.init();
     SmoothScroller.changeHash=true;
     SmoothScroller.speed=10;
+
+    new Drawer();
 });
---
+++ work/_posts/21-10-2015-sapo-videos.md
@@ -0,0 +1,17 @@
+---
+layout: work-post
+title:  "SAPO V&Iacute;DEOS"
+date:   21-10-2015
+author: "V&iacute;tor Teixeira"
+thumb: /img/videos_thumb.png
+thumb-alt-text: "SAPO Vídeos screenshot"
+intro: "SAPO Videos is the largest video hosting and sharing platform in Portugal. With the best media partners on board and a thriving community, SAPO Vídeos is the go to website to watch the best portuguese video content."
+---
+
+
+<!--Recently its interface was redesigned to simplify the interaction and improve presentation of content. The interface reserved to registered users also went through a major overall to simplify the process of uploading and managing video libraries. It's now also possible to use the website with any device."-->
+
+Check out [SAPO Vídeos][videos]{:target="_blank"} and enjoy.
+
+[videos]:      http://videos.sapo.pt
+
